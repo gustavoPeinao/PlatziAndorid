85c066eee7e2330c1958d0baf8739de705751e67
GustavoMamani <gustavopeinao@gmail.com>
2018-07-23 17:32:40 -0300

    Hasta video 17
    Metodo enviarDatos (MainActivity)
    Propiedad onClick="enviarDatos" (content_main)

--- app/src/main/java/com/gustavop/holamundo/MainActivity.java
+++ app/src/main/java/com/gustavop/holamundo/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gustavop.holamundo.model.Alumno;
 
@@ -44,8 +46,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-//        Cuando presione el boton "Enviar" nos de un msj toast
-        
+
+
+    }
+
+    //Cuando presione el boton "Enviar" nos de un msj toast
+    public void enviarDatos(View view){
+        EditText edtNombre = (EditText) findViewById(R.id.nombre);
+        //extraemos el nombre que contiene edtNombre
+        String nombre = edtNombre.getText().toString();
+
+        Toast.makeText(getBaseContext(), "Felicidades tu nombre es: " + nombre, Toast.LENGTH_LONG).show();
     }
 
     @Override
